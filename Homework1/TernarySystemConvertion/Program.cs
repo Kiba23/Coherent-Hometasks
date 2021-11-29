bb7a3bc0718ac8895b4eee33d3e6c620a8a57c9b
Anton Kibalka <anton.kibalka@gmail.com>
2021-11-29 20:14:34 +0200

    Homework1, TernarySystemConvertion and ISBN solutions were added.

---
+++ Homework1/TernarySystemConvertion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TernarySystemConvertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
