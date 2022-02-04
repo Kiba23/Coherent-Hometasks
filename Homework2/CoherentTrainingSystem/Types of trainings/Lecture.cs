b7912e6c2744fd028cfb8ff45c543fdcd9abb0ee
Anton <61226869+Kiba23@users.noreply.github.com>
2022-02-04 12:40:40 +0200

    Update Lecture.cs

--- Homework2/CoherentTrainingSystem/Types of trainings/Lecture.cs
+++ Homework2/CoherentTrainingSystem/Types of trainings/Lecture.cs
@@ -1,8 +1,21 @@
-﻿
+
 namespace CoherentTrainingSystem
 {
     public class Lecture : TrainingBase
     {
-        private string Topic = "";
+        public string Topic { get; set; }
+
+        public Lecture() { }
+        public Lecture(string description, string topic) : base(description)
+        {
+            Topic = topic;
+        }
+
+        public override TrainingBase Clone()
+        {
+            Lecture lecture = new Lecture(this.Description, this.Topic);
+
+            return lecture;
+        }
     }
 }
