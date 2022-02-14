1e84be065d04ee77f4da99b806bba9071eff97d1
Anton <anton.kibalka@gmail.com>
2022-02-14 20:25:21 +0200

    First breakpoint.
    Just initialized a basic logic. Main methods to add in future.

---
+++ Homework4/DiagonalMatrix/DiagonalMatrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalMatrix
+{
+    public class DiagonalMatrix<T>
+    {
+        private T[] DiagonalElements { get; }
+        public readonly int Size;
+
+
+        public DiagonalMatrix(int size, params T[] elements)
+        {
+            if (size <= 0 || elements.Length != size)
+            {
+                throw new ArgumentException("Size isn't appropriate.");
+            }
+
+            DiagonalElements = elements;
+            Size = elements.Length;
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                IsIndexerInputCorrect(i, j);
+
+                if (i != j)
+                {
+                    return default(T);
+                }
+                return DiagonalElements[i];
+            }
+            set
+            {
+                IsIndexerInputCorrect(i, j);
+
+                if (i != j)
+                {
+                    return;
+                }
+                DiagonalElements[i] = value;
+            }
+        }
+
+        private bool IsIndexerInputCorrect(int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= Size || j >= Size)
+            {
+                throw new IndexOutOfRangeException("Incorrect index.");
+            }
+            return true;
+        }
+    }
+}
