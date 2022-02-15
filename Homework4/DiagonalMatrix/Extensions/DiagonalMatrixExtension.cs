1f14a17bab5f4d6b38a52573608e92eff0f1b747
Anton <anton.kibalka@gmail.com>
2022-02-15 15:06:18 +0200

    Third breakpoint.
    Redisegned addition function for matrices with different dimensions. Added some tests in Program class.

--- Homework4/DiagonalMatrix/Extensions/DiagonalMatrixExtension.cs
+++ Homework4/DiagonalMatrix/Extensions/DiagonalMatrixExtension.cs
@@ -6,14 +6,35 @@ namespace DiagonalMatrix
         public delegate T MatrixAddition<T>(T element1, T element2);
 
         public static DiagonalMatrix<T> AddMatrices<T>(this DiagonalMatrix<T> matrix1, DiagonalMatrix<T> matrix2, 
-            MatrixAddition<T> addition) // Only for the same dimensional matrices, because there is no such requirement in the task description.
+            MatrixAddition<T> addition)
         {
-            int newSize = matrix1.Size >= matrix2.Size ? matrix1.Size : matrix2.Size;
-            T[] newElementsAfterAddition = new T[newSize];
+            T[] newElementsAfterAddition;
 
-            for (int i = 0; i < newSize; i++)
+            if (matrix1.Size == matrix2.Size) // For the matrices with the same dimension
             {
-                newElementsAfterAddition[i] = addition(matrix1[i, i], matrix2[i, i]);
+                int size = matrix1.Size;
+                newElementsAfterAddition = new T[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    newElementsAfterAddition[i] = addition(matrix1[i, i], matrix2[i, i]);
+                }
+            }
+            else // For the matrices with different dimension
+            {
+                var biggerMatrix = matrix1.Size > matrix2.Size ? matrix1 : matrix2;
+                var smallerMatrix = matrix1.Size < matrix2.Size ? matrix1 : matrix2;
+                newElementsAfterAddition = new T[biggerMatrix.Size];
+
+                for (int i = 0; i < smallerMatrix.Size; i++)
+                {
+                    newElementsAfterAddition[i] = addition(matrix1[i, i], matrix2[i, i]);
+                }
+
+                for (int i = smallerMatrix.Size; i < biggerMatrix.Size; i++)
+                {
+                    newElementsAfterAddition[i] = biggerMatrix[i, i];
+                }
             }
             return new DiagonalMatrix<T>(newElementsAfterAddition);
         }
--- Homework4/DiagonalMatrix/Program.cs
+++ Homework4/DiagonalMatrix/Program.cs
@@ -6,12 +6,19 @@ namespace DiagonalMatrix
     {
         static void Main(string[] args)
         {
-            var matrix1 = new DiagonalMatrix<int>(new int[] { 1, 44, 7, 3, 95 });
+            var matrix1 = new DiagonalMatrix<int>(new int[] { 1, 44, 7, 3, 95, 5, 6, 7 });
             var matrix2 = new DiagonalMatrix<int>(new int[] { 9, -7, 0, -3, 5 });
 
             var result = matrix1.AddMatrices(matrix2, NumericalAddition);
 
             Console.WriteLine(result);
+
+            var matrix3 = new DiagonalMatrix<float>(new float[] { 1.3f, 1.5678f, -3.43f });
+            var matrix4 = new DiagonalMatrix<float>(new float[] { -7.12f, 3.4f, 3.43f, 5f, 6.3f, 7.0f });
+
+            var result2 = matrix3.AddMatrices(matrix4, NumericalAddition);
+
+            Console.WriteLine(result2);
         }
 
         static T NumericalAddition<T>(T element1, T element2) // Only for addition between number types.
