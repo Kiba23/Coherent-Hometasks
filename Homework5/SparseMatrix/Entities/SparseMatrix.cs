5666c261b68f19fcf29fe4eaa0fbd2d10e74760d
Anton <anton.kibalka@gmail.com>
2022-02-22 17:20:08 +0200

    Completed Task1 in Homework5.
    Implemented all the things, all works good. Provided tests of all the features in Program class.

---
+++ Homework5/SparseMatrix/Entities/SparseMatrix.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SparseMatrixOperations.Entities
+{
+    public class SparseMatrix : IEnumerable<int>
+    {
+        private Dictionary<(int Column, int Row), int> MatrixElements = new Dictionary<(int, int), int>();
+        private (int Columns, int Rows) Size;
+
+
+        public SparseMatrix(int columns, int rows)
+        {
+            if (columns > 0 && rows > 0)
+            {
+                Size = (columns, rows);
+            }
+            else
+            {
+                throw new ArgumentException("Size of matrix isn't appropriate.");
+            }
+        }
+
+        public int this[int col, int row]
+        {
+            get
+            {
+                InputDimensionCheck(col, row);
+
+                if (IsZeroElement(col, row)) // case when there is no element in such indexes, so the value is zero
+                {
+                    return default(int);
+                }
+
+                var elementIndex = (col, row);
+                return MatrixElements.First(e => e.Key.Equals(elementIndex)).Value; // returning the value at corresponding indexes
+            }
+            set
+            {
+                InputDimensionCheck(col, row);
+
+                var elementIndex = (col, row);
+                MatrixElements.Add(elementIndex, value);
+            }
+        }
+
+        private bool InputDimensionCheck(int col, int row)
+        {
+            if (col >= 0 && col < Size.Columns && row >= 0 && row < Size.Rows)
+            {
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException("Input dimension isn't correct.");
+            }
+        }
+
+        public int GetCount(int x)
+        {
+            if (x == 0)
+            {
+                return Size.Columns * Size.Rows - MatrixElements.Values.Count;
+            }
+
+            return MatrixElements.Values.Where(v => v == x).Count();
+        }
+
+        public IEnumerable<(int, int, int)> GetNotZeroElements()
+        {
+            for (int i = 0; i < Size.Columns; i++)
+            {
+                for (int j = 0; j < Size.Rows; j++)
+                {
+                    if (!IsZeroElement(i, j))
+                    {
+                        yield return (i, j, this[i, j]);
+                    }
+                }
+            }
+        }
+
+        private bool IsZeroElement(int col, int row)
+        {
+            var elementIndex = (col, row);
+            if (MatrixElements.Any(e => e.Key.Equals(elementIndex))) // case when there is no element in such indexes, so the value is zero
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Size.Columns; i++) 
+            {
+                for (int j = 0; j < Size.Rows; j++)
+                {
+                    yield return this[i, j];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < Size.Columns; i++)
+            {
+                for (int j = 0; j < Size.Rows; j++)
+                {
+                    yield return this[i, j].ToString();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "\n";
+            foreach (var item in MatrixElements)
+            {
+                result += "Element at " + item.Key.Column + "," + item.Key.Row + " is " + item.Value + "\n";
+            }
+            return result + "of the matrix size - " + Size.Columns + "x" + Size.Rows;
+        }
+    }
+}
--- Homework5/SparseMatrix/Program.cs
+++ Homework5/SparseMatrix/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using SparseMatrixOperations.Entities;
 
-namespace SparseMatrix
+namespace SparseMatrixOperations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Initializing objects
+            var matrix = new SparseMatrix(3, 3);
+            matrix[0, 0] = 3;
+            matrix[1, 2] = 3;
+            matrix[2, 2] = 15;
+            matrix[1, 1] = 7;
+            #endregion
+
+            #region Testing get indexer
+            var val = matrix[0, 0];
+            var val2 = matrix[1, 2];
+            var val3 = matrix[2, 2];
+            #endregion
+
+            #region Testing GetEnumerator method
+            foreach (var elem in matrix)
+            {
+                Console.WriteLine(elem);
+            }
+            #endregion
+
+            #region Testing the output (ToString) and GetCount methods
+            Console.WriteLine("0 has occured - " + matrix.GetCount(0) + " times.");
+            Console.WriteLine("3 has occured - " + matrix.GetCount(3) + " times.");
+            Console.WriteLine(matrix + "\n");
+            #endregion
+
+            #region Testing GetNotZeroElements method
+            foreach (var elem in matrix.GetNotZeroElements())
+            {
+                Console.WriteLine(elem);
+            }
+            #endregion
         }
     }
 }
