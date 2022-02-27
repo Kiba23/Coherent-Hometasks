e5f469f23e5774aa4393bb32824520545d434667
Anton <anton.kibalka@gmail.com>
2022-02-27 21:38:31 +0200

    Finished HW6.
    From init to finish project. Implemented all the things, all works good.

---
+++ Homework6/JsonLogger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JsonLogger.TestObjects;
+using TrackingLibrary;
+
+namespace JsonLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var computer = new Computer("AMD RYZEN", 2020, "RYZEN4", "Nvidia 1050Ti", "ASUS G8790");
+
+            const string path = "C:\\Users\\pocht\\OneDrive\\Documents\\GitHub\\Coherent-Hometasks\\Homework6\\JsonLogger\\bin\\Debug\\net5.0\\Test.json";
+
+            var logger = new Logger(path);
+            logger.Track(computer);
+
+            Console.WriteLine($"Terminated successfully. Check the file on - {path}.");
+        }
+    }
+}
---
+++ Homework6/TrackingLibrary/TrackingClasses/TrackingEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TrackingLibrary.TrackingClasses
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class TrackingEntity : Attribute
+    {
+
+    }
+}
